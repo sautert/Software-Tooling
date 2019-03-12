428b79163b44198da7d98355a0e2d25cf2572274
Eliot <eliot_m@hotmail.fr>
2019-03-12 17:57:19 +0100

    <feat> : Adds a new script to install atom and its packages

---
+++ atom/install-atom-and-packages.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#==============================================================================#
+# This script is used to install all usefull atom plugins for Javascript,
+# Python and basic programming
+# (Created for UBUNTU 18.04)
+#==============================================================================#
+
+# Rights test
+ls /root 2> /dev/null 1> /dev/null
+if [ $? -ne 0 ]
+then
+	echo "<error> : the script has to be run has a sudo user"
+	exit 1
+fi
+
+# Atom installation
+sudo apt-get install atom
+
+# Package install
+sudo apm install autocomplete
+sudo apm install autocomplete-python
+sudo apm install autocomplete-javascript
+sudo apm install file-icons
+sudo apm install linter
+sudo apm install linter-eslint
+sudo apm install platformio-ide-terminal
